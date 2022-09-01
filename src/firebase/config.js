61210721cf398b3ce8b96c8be2c633939c35430b
brseghese <brseghese@gmail.com>
2022-09-01 12:46:48 -0300

    initial

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSWEDN-9IAOTBL9Fb1bg9IBxHbyk73yIk",
+  authDomain: "miniblog-8307f.firebaseapp.com",
+  projectId: "miniblog-8307f",
+  storageBucket: "miniblog-8307f.appspot.com",
+  messagingSenderId: "205752174223",
+  appId: "1:205752174223:web:973536069b5a57d30252fa",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
